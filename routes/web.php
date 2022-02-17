680c33d2dfebf2ca9ea325ae9d91f631818ec6b7
HekimWebDev <hekimyazrow@gmail.com>
2022-02-17 18:43:19 +0500

    firs commit

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Goutte\Client;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+
+    $client = new Client();
+    $scrawling = $client->request('GET', 'https://www.altinyildizclassics.com');
+    dump($scrawling->filter('.section-title')->text());
+
+
+    /* $scrawling->filter('.section-title')->each(function ($node) {
+        dump($node->text());
+    }); */
+
+    return view('welcome');
+});
