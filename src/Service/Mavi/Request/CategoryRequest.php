46aac864e78c7271226ea1742420bb3746462f95
Ysmayyl <Ysmayylhan0910@gmail.com>
2022-04-18 22:44:20 +0500

    Method for grab categories created

--- src/Service/Mavi/Request/CategoryRequest.php
+++ src/Service/Mavi/Request/CategoryRequest.php
@@ -3,6 +3,8 @@
 namespace Service\Mavi\Request;
 
 
+use function Symfony\Component\DomCrawler\first;
+
 trait CategoryRequest
 {
     public function getCategories()
@@ -20,17 +22,22 @@ trait CategoryRequest
 
                     $name = $n->filter('p')->first()->attr('data-category-name');
 
-                    $result[$name] = $n->filter('.dropdown-menu__list li a')->each(function ($q){
+                    $arr[] = $n->filter('.dropdown-menu__list li a')->each(function ($q){
 
                         $name = $q->attr('data-category-name');
+
                         $result[$name] = $q->attr('href');
 
                         return $result;
                     });
 
+                    foreach ($arr[0] as $key => $item){
+                        $result[$name][array_key_first($item)] = current($item);
+                    }
+
                     return $result;
                 });
-//                dd($arr[0]);
+
                 foreach ($arr[0] as $item){
                     $response = array_merge($response, $item);
                 }
